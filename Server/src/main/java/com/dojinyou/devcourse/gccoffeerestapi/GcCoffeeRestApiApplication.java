0af778e83e648d93dfe2c9da7dabb600cd6a161c
dojinyou <djyou128@gmail.com>
2022-05-04 15:47:57 +0900

    :tada: init: rest api project init

---
+++ Server/src/main/java/com/dojinyou/devcourse/gccoffeerestapi/GcCoffeeRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.dojinyou.devcourse.gccoffeerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcCoffeeRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcCoffeeRestApiApplication.class, args);
+    }
+
+}
---
+++ Server/src/test/java/com/dojinyou/devcourse/gccoffeerestapi/GcCoffeeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojinyou.devcourse.gccoffeerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcCoffeeRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
