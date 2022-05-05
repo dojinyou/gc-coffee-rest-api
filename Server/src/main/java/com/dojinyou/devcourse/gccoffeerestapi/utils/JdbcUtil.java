9d9197eef9882e882623477ce6150a599dba893e
dojinyou <djyou128@gmail.com>
2022-05-06 02:06:16 +0900

    :sparkles: feat(JdbcUtil): Jdbc 관련 util 기능 구현

---
+++ Server/src/main/java/com/dojinyou/devcourse/gccoffeerestapi/utils/JdbcUtil.java
@@ -0,0 +1,10 @@
+package com.dojinyou.devcourse.gccoffeerestapi.utils;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+public class JdbcUtil {
+    public static LocalDateTime toLocalDateTime(Timestamp timestamp) {
+        return timestamp != null ? timestamp.toLocalDateTime() : null;
+    }
+}
