7ec493e2afb20992c98174a180f70e0d1be2b1f6
dojinyou <djyou128@gmail.com>
2022-05-07 01:58:52 +0900

    :label: feat(ProductService): return type Optional에서 domain 객체로 변경

--- Server/src/main/java/com/dojinyou/devcourse/gccoffeerestapi/product/ProductService.java
+++ Server/src/main/java/com/dojinyou/devcourse/gccoffeerestapi/product/ProductService.java
@@ -4,16 +4,15 @@ import com.dojinyou.devcourse.gccoffeerestapi.product.domain.Category;
 import com.dojinyou.devcourse.gccoffeerestapi.product.domain.Product;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ProductService {
     void insert(Product product);
 
     List<Product> findAll();
 
-    Optional<Product> findById(long id);
+    Product findById(long id);
 
-    Optional<Product> findByName(String name);
+    Product findByName(String name);
 
     List<Product> findAllByCategory(Category category);
 
