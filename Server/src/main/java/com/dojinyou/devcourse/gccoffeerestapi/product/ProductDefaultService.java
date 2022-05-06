13f5000a481ffaeab955def5b997597f48cb4cf9
dojinyou <djyou128@gmail.com>
2022-05-07 02:01:48 +0900

    :sparkles: feat(ProductService): return type 변경사항 구현 및 NotFoundException 추가

--- Server/src/main/java/com/dojinyou/devcourse/gccoffeerestapi/product/ProductDefaultService.java
+++ Server/src/main/java/com/dojinyou/devcourse/gccoffeerestapi/product/ProductDefaultService.java
@@ -1,11 +1,11 @@
 package com.dojinyou.devcourse.gccoffeerestapi.product;
 
+import com.dojinyou.devcourse.gccoffeerestapi.common.exception.NotFoundException;
 import com.dojinyou.devcourse.gccoffeerestapi.product.domain.Category;
 import com.dojinyou.devcourse.gccoffeerestapi.product.domain.Product;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class ProductDefaultService implements ProductService {
@@ -19,7 +19,7 @@ public class ProductDefaultService implements ProductService {
     @Override
     public void insert(Product product) {
         if (product.getId() != 0) {
-            throw new IllegalArgumentException("생성되는 상품은 Id가 0이어야 합니다.");
+            throw new IllegalArgumentException("생성을 위한 상품은 Id가 0 이어야 합니다.");
         }
         productRepository.insert(product);
     }
@@ -30,19 +30,21 @@ public class ProductDefaultService implements ProductService {
     }
 
     @Override
-    public Optional<Product> findById(long id) {
+    public Product findById(long id) {
         if (id <= 0) {
-            throw new IllegalArgumentException("id값은 양의 정수로만 찾을 수 있습니다.");
+            throw new NotFoundException(id + "의 id를 가진 상품을 찾을 수 없습니다.");
         }
-        return productRepository.findById(id);
+        return productRepository.findById(id).orElseThrow(NotFoundException::new);
+
     }
 
     @Override
-    public Optional<Product> findByName(String name) {
+    public Product findByName(String name) {
         if (name == null) {
             throw new IllegalArgumentException("상품의 이름이 없습니다.");
         }
-        return productRepository.findByName(name);
+        return productRepository.findByName(name)
+                                .orElseThrow(() -> new NotFoundException(name + "(이)라는 이름을 가진 상품을 찾을 수 없습니다."));
     }
 
     @Override
@@ -59,8 +61,8 @@ public class ProductDefaultService implements ProductService {
             throw new IllegalArgumentException("입력된 상품이 없습니다.");
         }
 
-        if (product.getId() == 0) {
-            throw new IllegalArgumentException("id값이 0인 상품은 update 할 수 없습니다.");
+        if (product.getId() < 1) {
+            throw new IllegalArgumentException("id가 양의 정수가 아닌 상품은 update 할 수 없습니다.");
         }
 
         productRepository.update(product);
@@ -69,7 +71,7 @@ public class ProductDefaultService implements ProductService {
     @Override
     public void deleteById(long id) {
         if (id <= 0) {
-            throw new IllegalArgumentException("id값은 양의 정수로만 찾을 수 있습니다.");
+            throw new IllegalArgumentException("id는 양의 정수로만 찾을 수 있습니다.");
         }
         productRepository.deleteById(id);
 
