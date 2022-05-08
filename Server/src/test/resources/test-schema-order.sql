c87aee160a3cf3525f92bcd6d141b80950000d5b
dojinyou <djyou128@gmail.com>
2022-05-08 16:06:26 +0900

    :bento: chore: test용 sql 추가

---
+++ Server/src/test/resources/test-schema-order.sql
@@ -0,0 +1,42 @@
+CREATE TABLE products
+(
+    id          BIGINT          PRIMARY KEY AUTO_INCREMENT,
+    name        VARCHAR(20)     NOT NULL UNIQUE,
+    category    VARCHAR(50)     NOT NULL,
+    price       BIGINT          NOT NULL,
+    description VARCHAR(500)    DEFAULT NULL,
+    created_at  TIMESTAMP(3)    NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at  TIMESTAMP(3)    NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+    is_deleted  TINYINT         NOT NULL DEFAULT 0
+);
+
+CREATE TABLE orders
+(
+    id              BIGINT          PRIMARY KEY AUTO_INCREMENT,
+    email           VARCHAR(50)     NOT NULL,
+    address         VARCHAR(200)    NOT NULL,
+    postcode        VARCHAR(200)    NOT NULL,
+    order_status    VARCHAR(50)     NOT NULL,
+    created_at      TIMESTAMP(3)    NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at      TIMESTAMP(3)    NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3)
+);
+
+CREATE TABLE order_products
+(
+    id              BIGINT          PRIMARY KEY AUTO_INCREMENT,
+    order_id        BIGINT          NOT NULL,
+    product_id      BIGINT          NOT NULL,
+    quantity        INT             NOT NULL,
+    created_at      TIMESTAMP(3)    NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    updated_at      TIMESTAMP(3)    NOT NULL DEFAULT CURRENT_TIMESTAMP(3) ON UPDATE CURRENT_TIMESTAMP(3),
+    CONSTRAINT fk_order_product_to_order FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE,
+    CONSTRAINT fk_order_product_to_product FOREIGN KEY (product_id) REFERENCES products(id)
+);
+
+CREATE INDEX idx_order_products_order_id ON order_products(order_id);
+
+-- INSERT INTO products(name, category, price, description)
+-- VALUES('test1', 'TEA', 2000, null);
+--
+-- INSERT INTO products(name, category, price, description)
+-- VALUES('test2', 'COFFEE', 3000, '맛있는 커피');
