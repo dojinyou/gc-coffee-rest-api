b25028f53d93f2c6c334fd311c1cb69b2cfecafa
dojinyou <djyou128@gmail.com>
2022-05-08 16:23:49 +0900

    :sparkles: feat(OrderService): Order Interface 정의

---
+++ Server/src/main/java/com/dojinyou/devcourse/gccoffeerestapi/order/OrderService.java
@@ -0,0 +1,7 @@
+package com.dojinyou.devcourse.gccoffeerestapi.order;
+
+import com.dojinyou.devcourse.gccoffeerestapi.order.domain.Order;
+
+public interface OrderService {
+    void create(Order order);
+}
